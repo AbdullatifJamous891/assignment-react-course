c21d850dd41d04de7403b2faabc3428ea49e8f70
AbdullatifJamous891 <Abdullatif.jamous891@gmail.com>
2020-06-01 02:52:16 +0300

    use react Hook

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,10 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import UserOutput from "./UserOutput/UserOutput";
+import UserInput from "./UserInput/UserInput";
 
 function App() {
+  const [usernameState, setUsernameState] = useState({
+    username: "Abdullatif Jamous"
+  });
+
+  const changeUserNameHandler = event => {
+    setUsernameState({
+      username: event.target.value
+    });
+  };
+
   return (
     <div className="App">
-      <p>Assignment</p>
+      <UserOutput username={usernameState.username} />
+      <UserInput click={changeUserNameHandler} />
     </div>
   );
 }
